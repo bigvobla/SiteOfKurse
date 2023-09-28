5de9bc4caa13d28258ef434d551d67eb6df87e36
bigvobla <simplecovachcsgo@gmail.com>
2023-09-28 10:30:27 +0600

    second work

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from main import views
 
+
 urlpatterns = [
     path('login', views.login),
     # path('', views.login),
+    path('profile', views.profle),
+    path('index',views.index)
 ]
+
+
--- main/views.py
+++ main/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from main import models
 
 
 def login(request):
     return render(request, 'account/login.html')
+
+def profle(request):
+    return render(request, 'account/profile.html')
+
+def index(request):
+    name = request.POST.get("name")
+
+    models.Product.objects.filter(name=name)
+
+    return render(request, 'products/index.html')
